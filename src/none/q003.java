8fb46fa1e5417d3495619e6f0ffeb60b2bfdd842
kalildeoliveira <69366429+kalildeoliveira@users.noreply.github.com>
2021-10-12 14:48:39 -0300

    Add files via upload

---
+++ src/none/q003.java
@@ -0,0 +1,24 @@
+package none;
+import java.util.*;
+public class q003 {
+
+	/**
+	 * lê e valida
+	 * 	a) nome com mais de 3 caracteres;
+	 * 	b) idade entre 0 e 150;
+	 * 	c) salário maior que 0;
+	 * 	d) sexo com 'f' ou 'm';
+	 * 	e) Estado civil 's', 'c', 'v', 'd'; 
+	 */
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		String nome="";
+		double salario=0;
+		char f='f', m='m', s='s', c='c', v='v', d='d';
+		
+		// a)
+		do {
+			
+		}
+	}
+}
