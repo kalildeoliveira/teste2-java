7d6e9202055e6dce1201cde369e80bfbc10decfd
kalildeoliveira <69366429+kalildeoliveira@users.noreply.github.com>
2021-10-13 03:56:21 -0300

    Add files via upload

--- src/none/q006.java
+++ src/none/q006.java
@@ -9,17 +9,18 @@ public class q006 {
 	public static void main(String[] args) {
 		Scanner entrada = new Scanner((System.in));
 		String opcao="", r="r";
-		int [] numero = new int [52];
-		System.out.println("Digite \"r\" para gerar relação de números ímpares de 1 a 50");
+		System.out.println("Digite \"r\" para gerar relação de números ímpares de 1 a 50" +
+				" ou outra letra para encerrar o programa");
 		System.out.print("Opção: ");
 		opcao = entrada.next();
 		if (opcao.equals(r)) {
-		for (int i=1; i<=50;) {
-			System.out.print("\n "+i+": ");
-			i=i+2;
-			numero[i] = i;
-		}} else {
+			for (int i=1; i<=50;) {
+				System.out.print("\n "+i+" ");
+				i=i+2;
+			}
 			System.out.println("\n\nFim do Programa.");
+		} else {
+			System.out.println("\n\nPrograma encerrado.");
 		}
 	}
 }
