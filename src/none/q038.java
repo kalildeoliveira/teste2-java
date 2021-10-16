d0c6120c45c3f5cdc85bebccb487e2bb7438c756
kalildeoliveira <69366429+kalildeoliveira@users.noreply.github.com>
2021-10-16 17:16:49 -0300

    Add files via upload

--- src/none/q038.java
+++ src/none/q038.java
@@ -1,5 +1,6 @@
 package at3;
 import java.util.*;
+import java.lang.Math.*;
 public class q038 {
 
 	/**
@@ -7,6 +8,28 @@ public class q038 {
 	 */
 
 	public static void main (String[]args) {
-		Scanner entrada = new Scanner(System.in);
+		Scanner entrada = new Scanner(System.in);		
+		int numero=0, auxiliar=0, novoNumero=0, contador=0;
+		double troca=0;
+		boolean ficarNoLoop=true;
+		System.out.println("Informe um número inteiro.");
+		System.out.println("Número: ");
+		numero=entrada.nextInt();
+		auxiliar=numero;
+		while (ficarNoLoop) {
+			auxiliar=auxiliar/10;
+			contador++;			
+			if (auxiliar==0) {
+				ficarNoLoop=false;
+			}
+		}
+		auxiliar=numero;
+		for (int i=1; i<=contador; i++) {
+			auxiliar=numero%10;
+			novoNumero=auxiliar;
+			auxiliar=numero;
+			numero=auxiliar/10;
+			System.out.print(novoNumero);
+		}
 	}
 }
